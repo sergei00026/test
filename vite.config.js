e88723b18533dcb17bf2fccfb81bd740cf612aa8
sergei00026 <sergei57fx@mail.ru>
2025-06-06 09:42:08 +0300

    4

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [preact()],
+    base: '/test/',
     server: {
         watch: {
             usePolling: true
